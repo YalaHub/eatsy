0eabc6d5f0bc6d3065847b99f128a17ebb8b1324
Adam Yala <adamyala@mit.edu>
2015-07-14 12:51:17 -0400

    Eatsy: Paired with eyin to add the styles

---
+++ client/includes/header.js
@@ -0,0 +1,21 @@
+function hasClass(element, className) {
+    return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+}
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 150,
+            header = document.getElementById("header");
+            content = document.getElementById("content");
+        if (distanceY > shrinkOn) {
+            header.classList.add("smaller");
+            content.classList.add("content-smaller");
+        } if (distanceY < 130) {
+            if (hasClass(header,"smaller")) {
+            	header.classList.remove("smaller");
+            	content.classList.remove("content-smaller");
+            }
+        }
+    });
+}
+window.onload = init();
