81b36a24820d39272029ed8dc4508c876609fd2b
Adam Yala <adamyala@mit.edu>
2015-07-14 15:32:32 -0400

    Added functionality to submit a new eatsery and edit an existing one for admins

---
+++ client/templates/eatsery/editEatsery.js
@@ -0,0 +1,28 @@
+Template.editEatsery.onCreated(function() {
+  Session.set('editEatseryErrors', {});
+});
+
+Template.editEatsery.events({
+	'submit form': function(e) {
+		e.preventDefault();
+
+		var eatseryAttributes = {
+			name: $(e.target).find('[name=name]').val(),
+			location: $(e.target).find('[name=address]').val(),
+			distance: 2
+		};
+
+		Eatsery.update( this._id, {$set: eatseryAttributes});
+		Router.go('eatseryPage', {_id: this._id});
+
+	}
+});
+
+Template.editEatsery.helpers({
+  errorMessage: function(field) {
+    return Session.get('eatserySubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('eatserySubmitErrors')[field] ? 'has-error' : '';
+  }
+});
